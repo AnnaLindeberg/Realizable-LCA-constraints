b4e5651e7138c402c8df0d348bde974a086647d7
captainutas <74547587+captainutas@users.noreply.github.com>
2025-11-04 16:19:38 +0100

    added exception raising for number of elements in each constraint and constraint with elements not in the leafset

--- parse_input.py
+++ parse_input.py
@@ -3,24 +3,38 @@ import csv
 
 
 def read_constraints_csv(filename: str) -> tuple[set, dict]:
-
     with open(filename) as file:
 
         rdr = csv.reader(file)
 
         leafs = rdr.__next__()
         leafs = {leaf.strip() for leaf in leafs}
-        
+
         constraints = []
-        for line in rdr:
+        for line in enumerate(rdr):
             constraints.append(line)
     
         R = {}
-        for a, b, c, d in constraints:
+
+        for i, constraint in enumerate(constraints):
+            if len(constraint) < 4:
+                raise ValueError(f"Wrong formatting for constraint nr {i}, too few values. Constraint abRcd should be written a,b,c,d")
+            if len(constraint) > 4:
+                raise ValueError(f"Wrong formatting for constraint nr {i}, too many values. Constraint abRcd should be written a,b,c,d")
+            
+            a, b, c, d = constraint
             a, b, c, d = a.strip(), b.strip(), c.strip(), d.strip()
+
+            if (a not in leafs 
+                or b not in leafs 
+                or c not in leafs 
+                or d not in leafs): 
+                raise ValueError(f"Constraint nr {i} mention a leaf not in the leaf set. List all leaves on the first line. Constraint abRcd should be written a,b,c,d")
+            
             if (a,b) not in R:
                 R[(a,b)] = set()
             R[(a,b)].add((c,d))
+            
 
         return leafs, R
 
